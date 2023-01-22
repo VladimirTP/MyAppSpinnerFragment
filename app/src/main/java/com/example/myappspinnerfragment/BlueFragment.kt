57930776b324f6096e9e80e5c6112afc58bf563c
TamashovVladimir <v.tamashov@gmail.com>
2023-01-22 22:41:20 +0300

    homework

---
+++ app/src/main/java/com/example/myappspinnerfragment/BlueFragment.kt
@@ -0,0 +1,21 @@
+package com.example.myappspinnerfragment
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+class BlueFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_blue, container, false)
+    }
+
+    companion object {
+        fun newInstance() = BlueFragment()
+    }
+}
